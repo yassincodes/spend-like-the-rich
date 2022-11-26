bd7737a44f5059d7015b6b040ddfe1cf2fb1a50f
yassin <meddyassin@hotmail.com>
2022-11-26 23:30:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <>
+      <header>
+        <div>spend like the rich</div>
+        <div></div>
+      </header>
+      <div className="container">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
